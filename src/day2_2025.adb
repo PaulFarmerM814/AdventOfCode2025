f327e82242ecb75dad3caecb0b62a7f37aae67c4
PaulFarmerM814 <33911185+PaulFarmerM814@users.noreply.github.com>
2025-12-15 18:59:09 +0000

    Update day2_2025.adb

--- src/day2_2025.adb
+++ src/day2_2025.adb
@@ -30,12 +30,17 @@ package body day2_2025 is
       range_end_len   : Integer;
 
       invalid_range_sum : Long_Long_Integer := 0;
+      invalid_range_sum_pt2 : Long_Long_Integer := 0;
 
       range_valid : boolean;
 
       start_pos : integer;
       divider   : integer := -1;
       comma     : integer;
+
+      matching_at_end : boolean := false;
+      current_substr_len : integer;
+      matching_substr : boolean;
 
    begin
 
@@ -96,7 +101,7 @@ package body day2_2025 is
                               x_string : constant string := x'Img(2..x'Img'Length);
                            begin
                               this_range_loop:
-                              for y in x_string'first..x_string'first + x_string'Length/2 -1
+                              for y in x_string'First..x_string'First + x_string'Length/2 -1
                               loop
                                  if (x_string(y) /= x_string(x_string'Length/2 + y))
                                  then
@@ -107,19 +112,74 @@ package body day2_2025 is
                            end;
                            if (not range_valid)
                            then
-                              ada.text_io.put_line("invalid_id " & x'img);
+                              ada.text_io.put_line("invalid_id " & x'Img);
                               invalid_range_sum := invalid_range_sum + x;
                            end if;
                         end if;
                      end loop;
 
                   end if;
+
+                  for x in range_start..range_end
+                  loop
+
+                     declare
+                        x_string : constant string := x'Img(2..x'Img'Length);
+                     begin
+                        current_substr_len := x_string'Length - 1;
+
+                        matching_at_end := false;
+
+                        substr_loop:
+                        while current_substr_len >= 1 and then not matching_at_end
+                        loop
+                           declare
+                              current_substr : constant string := x_string(x_string'First..x_string'First+current_substr_len-1);
+                              current_substr_start : integer := x_string'First+current_substr_len;
+                           begin
+
+                              matching_substr := true;
+
+                              while matching_substr
+                              loop
+                                 if current_substr_start+current_substr_len-1 > x_string'Last
+                                 then
+                                    matching_substr := false;
+                                    matching_at_end := false;
+                                 elsif x_string(current_substr_start..current_substr_start+current_substr_len-1) = current_substr
+                                 then
+                                    if (current_substr_start+current_substr_len-1 = x_string'Last)
+                                    then
+                                       matching_substr := false;
+                                       matching_at_end := true;
+                                    end if;
+                                 else
+                                    matching_at_end := false;
+                                    matching_substr := false;
+                                 end if;
+
+                                 current_substr_start := current_substr_start + current_substr_len;
+                              end loop;
+                           end;
+                           current_substr_len := current_substr_len - 1;
+                        end loop substr_loop;
+                     end;
+
+                     if (matching_at_end)
+                     then
+                        ada.text_io.put_line("invalid_id " & x'Img & " current_substr_len " & current_substr_len'Img);
+                        invalid_range_sum_pt2 := invalid_range_sum_pt2 + x;
+                     end if;
+
+                  end loop;
+
                end if;
             end loop;
          end;
       end loop;
 
       Ada.Text_IO.Put_Line ("invalid_range_sum " & invalid_range_sum'Image);
+      Ada.Text_IO.Put_Line ("invalid_range_sum_pt2 " & invalid_range_sum_pt2'Image);
 
       Ada.Text_IO.Close(Input_File);
 
