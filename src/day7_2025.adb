10d04a4c4b09ec3769bbd1fd723a7aee0dc36a8d
PaulFarmerM814 <33911185+PaulFarmerM814@users.noreply.github.com>
2025-12-19 18:24:24 +0000

    Update day7_2025.adb

--- src/day7_2025.adb
+++ src/day7_2025.adb
@@ -14,6 +14,7 @@ package body day7_2025 is
    input : input_type.Instance;
 
    splitter_count : Long_Long_Integer := 0;
+   beam_count : Long_Long_Integer := 0;
 
    procedure Get_Input (S : Ada.Text_IO.File_Type)
    is
@@ -47,8 +48,50 @@ package body day7_2025 is
       declare
          subtype input_str_type is string(1..ada.strings.unbounded.length(input.table(1)));
          type input_array_type is array(1..input.last) of input_str_type;
+         type input_int_type is array(1..ada.strings.unbounded.length(input.table(1))) of Long_Long_Integer;
+         type input_int_array_type is array(1..input.last) of input_int_type;
 
          input_array : input_array_type;
+         input_array_pt2 : input_array_type;
+         input_int_array_pt2 : input_int_array_type := (others => (others => 0));
+
+         procedure beam(start_x : in integer;
+                        start_y : in integer)
+         is
+         begin
+            if (input_array_pt2(start_y)(start_x) = '^')
+            then
+               beam(start_x - 1, start_y + 1);
+               beam(start_x + 1, start_y + 1);
+            elsif (input_array_pt2(start_y)(start_x) = '.' or else input_array_pt2(start_y)(start_x) = 'S')
+            then
+               if (start_y >= input_array_pt2'Last)
+               then
+                  beam_count := @ + 1;
+                  --  Ada.Text_IO.Put_Line ("beam_count " & beam_count'Image);
+               else
+                  if (start_y + 1 >= input_array_pt2'Last)
+                  then
+                     beam_count := @ + 1;
+                     --  Ada.Text_IO.Put_Line ("beam_count " & beam_count'Image);
+                  else
+                     for y in start_y + 1..input_array_pt2'Last
+                     loop
+                        if input_array_pt2(y)(start_x) = '^'
+                        then
+                           beam(start_x - 1, y + 1);
+                           beam(start_x + 1, y + 1);
+                           exit;
+                        elsif (y = input_array_pt2'Last)
+                        then
+                           beam_count := @ + 1;
+                           --  Ada.Text_IO.Put_Line ("beam_count " & beam_count'Image);
+                        end if;
+                     end loop;
+                  end if;
+               end if;
+            end if;
+         end beam;
 
       begin
          for x in input_type.first..input.last
@@ -62,7 +105,7 @@ package body day7_2025 is
             loop
                if (input_array(x)(y) = 'S')
                then
-                  input_array(x+1)(y) := '|';
+                  input_array(x)(y) := '|';
                elsif (input_array(x)(y) = '^')
                then
                   if (input_array(x-1)(y) = '|')
@@ -76,8 +119,6 @@ package body day7_2025 is
 
                      input_array(x)(y-1) := '|';
                      input_array(x)(y+1) := '|';
-                     input_array(x+1)(y-1) := '|';
-                     input_array(x+1)(y+1) := '|';
                   end if;
                elsif (input_array(x)(y) = '|')
                then
@@ -98,9 +139,67 @@ package body day7_2025 is
          loop
             Ada.Text_IO.Put_Line (input_array(x));
          end loop;
+
+         Ada.Text_IO.Put_Line ("splitter_count " & splitter_count'Image);
+
+         input_array_pt2 := input_array;
+
+         for x in input_array_pt2'Range
+         loop
+            for y in input_array_pt2(x)'Range
+            loop
+               if (input_array_pt2(x)(y) = 'S')
+               then
+                  null;
+               elsif (input_array_pt2(x)(y) = '^')
+               then
+                  null;
+               elsif (input_array_pt2(x)(y) = '|')
+               then
+                  if (x = input_array_pt2'First)
+                  then
+                     input_int_array_pt2(x)(y) := 1;
+                  else
+                     input_int_array_pt2(x)(y) := input_int_array_pt2(x-1)(y);
+                  end if;
+
+                  if (y /= input_array_pt2(x)'First and then input_array_pt2(x)(y-1) = '^')
+                  then
+                     input_int_array_pt2(x)(y) := @ + input_int_array_pt2(x-1)(y-1);
+                  end if;
+                  if (y /= input_array_pt2(x)'Last and then input_array_pt2(x)(y+1) = '^')
+                  then
+                     input_int_array_pt2(x)(y) := @ + input_int_array_pt2(x-1)(y+1);
+                  end if;
+               elsif (input_array_pt2(x)(y) = '.')
+               then
+                  null;
+               end if;
+            end loop;
+         end loop;
+
+         for y in input_int_array_pt2(input_int_array_pt2'Last)'Range
+         loop
+            beam_count := @ + input_int_array_pt2(input_int_array_pt2'Last)(y);
+         end loop;
+
+         --  why do I never learn and try recursion...... doh!!!!
+         outer:
+         for x in input_array_pt2'Range
+         loop
+            for y in input_array_pt2(x)'Range
+            loop
+               if (input_array_pt2(x)(y) = 'S')
+               then
+                  --  beam(y,x);
+                  exit outer;
+               end if;
+            end loop;
+         end loop outer;
+
       end;
 
-      Ada.Text_IO.Put_Line ("splitter_count " & splitter_count'Image);
+      Ada.Text_IO.Put_Line ("beam_count " & beam_count'Image);
 
       Ada.Text_IO.Close(Input_File);
 
